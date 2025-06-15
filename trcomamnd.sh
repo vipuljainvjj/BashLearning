b6b7c4def3ca86bc8021c1059f99b902bea20ae1
Vipul Jain <vipuljain3456@gmail.com>
2025-06-16 00:19:50 +0530

    tr command

---
+++ trcomamnd.sh
@@ -0,0 +1,15 @@
+# In a given fragment of text, replace all parentheses () with box brackets [].
+
+tr "()" "[]"
+
+###################################################################
+
+# In a given fragment of text, delete all the lowercase characters a-z.
+
+tr -d [a-z]
+
+###################################################################
+
+# In a given fragment of text, replace all sequences of multiple spaces with just one space.
+
+tr -s " "

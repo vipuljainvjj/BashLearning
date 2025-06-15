4055c1b0fd699a299284e6f79b17a719524a8f2b
Vipul Jain <vipuljain3456@gmail.com>
2025-06-16 00:19:26 +0530

    head tail command example

---
+++ headtailcommand.sh
@@ -0,0 +1,27 @@
+# Display the first 20 lines of an input file.
+
+head -n 20
+
+###################################################################
+
+# Display the first 20 characters of an input file.
+
+head -c 20
+
+###################################################################
+
+# Display the lines (from line number 12 to 22, both inclusive) of a given text file.
+
+head -n 22 | tail -n 11
+
+###################################################################
+
+# Display the last 20 lines of an input file.
+
+tail -n 20
+
+###################################################################
+
+# Display the last 20 characters of an input file.
+
+tail -c 20
